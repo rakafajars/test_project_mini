e95cc27da77b6f78c5080da0b69af2035e1e4ae4
raka <rakafajar18@gmail.com>
2022-08-09 00:14:36 +0700

    add utils colors

--- lib/ui/home/home_page.dart
+++ lib/ui/home/home_page.dart
@@ -2,6 +2,7 @@ import 'package:blog_apps/bloc/blog/blog_bloc.dart';
 import 'package:blog_apps/model/response_blog.dart';
 import 'package:blog_apps/ui/home/detail_page.dart';
 import 'package:blog_apps/ui/home/widget/item_blog.dart';
+import 'package:blog_apps/utils/theme.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -36,6 +37,44 @@ class _HomePageState extends State<HomePage> {
               List<ResponseBlog> _listBlog = state.responseBlog;
               return Column(
                 children: [
+                  const SizedBox(height: 10),
+                  const Padding(
+                    padding: EdgeInsets.symmetric(
+                      horizontal: 20,
+                    ),
+                    child: TextField(
+                      textAlign: TextAlign.start,
+                      decoration: InputDecoration(
+                        suffixIcon: Icon(
+                          Icons.search,
+                          size: 16,
+                        ),
+                        enabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide(
+                            color: AppColors.colorF0F1FA,
+                            width: 1,
+                          ),
+                          borderRadius: BorderRadius.all(
+                            Radius.circular(
+                              16,
+                            ),
+                          ),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide(
+                            color: AppColors.colorFF3A44,
+                            width: 1,
+                          ),
+                          borderRadius: BorderRadius.all(
+                            Radius.circular(
+                              8,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 18),
                   Expanded(
                     child: ListView.builder(
                       itemCount: _listBlog.length,
---
+++ lib/utils/theme.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color colorF0F1FA = Color.fromARGB(255, 26, 28, 41);
+  static const Color color818181 = Color(0xFF818181);
+  static const Color colorFF3A44 = Color(0xFFFF3A44);
+  static const Color color0080FF = Color(0xFF0080FF);
+  static const Color color2E0505 = Color(0xFF2E0505);
+}
